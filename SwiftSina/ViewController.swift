5b691a4225d0791548a7d2736c1b35946a08dfae
yuzhouheike <26108724@qq.com>
2016-08-02 08:44:42 +0800

    Initial Commit

---
+++ SwiftSina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSina
+//
+//  Created by 王磊磊 on 16/8/2.
+//  Copyright © 2016年 丰月科技. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
